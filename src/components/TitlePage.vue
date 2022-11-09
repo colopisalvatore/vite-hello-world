c72d26c437856a8020303153fa0fe547300f9030
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-09 19:06:26 +0100

    completato

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+  
+<script>
+export default {
+    name: 'TitlePage',
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+  
+<style lang="css" scoped>
+h1 {
+    text-align: center;
+}
+</style>
